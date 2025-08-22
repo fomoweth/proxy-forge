c1aa8f9fd794404d149637264d771140f8621a91
Ryan <rkim@greenwood.app>
2025-08-22 11:41:38 -0700

    Updated deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,63 +7,52 @@ import {ProxyForge} from "src/ProxyForge.sol";
 contract Deploy is Script {
 	using stdJson for string;
 
-	bytes32 internal constant FACTORY_SALT = keccak256(bytes("ProxyForge"));
+	string internal constant DEFAULT_MNEMONIC = "test test test test test test test test test test test junk";
+	bytes32 internal constant DEFAULT_SALT = keccak256(bytes("ProxyForge"));
 
-	string internal constant TEST_MNEMONIC = "test test test test test test test test test test test junk";
+	address internal broadcaster;
+	bytes32 internal salt;
 
-	modifier broadcast(string memory chainAlias, address broadcaster) {
+	modifier broadcast(string memory chainAlias) {
 		vm.createSelectFork(chainAlias);
 		vm.startBroadcast(broadcaster);
 		_;
 		vm.stopBroadcast();
 	}
 
-	function run() external {
-		bytes32 salt = vm.envOr({name: "FACTORY_SALT", defaultValue: FACTORY_SALT});
-		address deployer = configureBroadcaster();
-
-		string[] memory chains = vm.envString("CHAINS", ",");
-		for (uint256 i; i < chains.length; ++i) deployToChain(chains[i], deployer, salt);
-	}
-
-	function deployToChain(
-		string memory chainAlias,
-		address deployer,
-		bytes32 salt
-	) internal broadcast(chainAlias, deployer) {
-		Chain memory chain = getChain(block.chainid);
-
-		string memory path = string.concat("./deployments/", vm.toString(block.chainid), ".json");
-
-		console.log("======================================================================");
-		console.log("Deploying on:", chain.name);
-
-		address instance = address(new ProxyForge{salt: salt}());
-
-		string memory json = "json";
-		json.serialize("chainAlias", chainAlias);
-		json.serialize("chainId", chain.chainId);
-		json.serialize("address", vm.toString(instance));
-		json.serialize("salt", vm.toString(salt));
-		json.serialize("blockNumber", block.number);
-		json.serialize("timestamp", block.timestamp);
-		json = json.serialize("deployer", deployer);
-		json.write(path);
-
-		console.log("Deployed at:", instance);
-		console.log("File Path:", path);
-		console.log("======================================================================");
-	}
-
-	function configureBroadcaster() internal returns (address) {
+	function setUp() public {
 		uint256 privateKey = vm.envOr({
 			name: "PRIVATE_KEY",
 			defaultValue: vm.deriveKey({
-				mnemonic: vm.envOr({name: "MNEMONIC", defaultValue: TEST_MNEMONIC}),
+				mnemonic: vm.envOr({name: "MNEMONIC", defaultValue: DEFAULT_MNEMONIC}),
 				index: uint8(vm.envOr({name: "EOA_INDEX", defaultValue: uint256(0)}))
 			})
 		});
 
-		return vm.rememberKey(privateKey);
+		broadcaster = vm.rememberKey(privateKey);
+		salt = vm.envOr({name: "FACTORY_SALT", defaultValue: DEFAULT_SALT});
+	}
+
+	function run() external {
+		string[] memory chains = vm.envString("CHAINS", ",");
+		for (uint256 i; i < chains.length; ++i) deployOnChain(chains[i]);
+	}
+
+	function deployOnChain(string memory chainAlias) internal broadcast(chainAlias) {
+		string memory path = string.concat("./deployments/", vm.toString(block.chainid), ".json");
+		address instance = address(new ProxyForge{salt: salt}());
+
+		string memory json = "json";
+		json.serialize("address", vm.toString(instance));
+		json.serialize("blockNumber", block.number);
+		json.serialize("salt", vm.toString(salt));
+		json = json.serialize("timestamp", block.timestamp);
+		json.write(path);
+
+		console.log("======================================================================");
+		console.log("Chain ID:", block.chainid);
+		console.log("Deployed at:", instance);
+		console.log("File Path:", path);
+		console.log("======================================================================");
 	}
 }
